a337ed203948974a1c40d09b2b2c5978c2f04744
Ronna Mae Firmo <ronnwiseley@gmail.com>
2018-03-10 22:53:13 +0800

    feat(Receipt.jsx): add Receipt component

--- Receipt.jsx
+++ Receipt.jsx
@@ -1,10 +1,73 @@
 import React, { Component } from 'react'
+import { Card, Table, Icon, List, Image, Divider, Segment } from 'semantic-ui-react'
 
 export default class Body extends Component {
   render() {
+    const { reports } = this.props
+    
     return (
-      <div>
-      </div>
+      <Segment style={{ paddingTop: '1em' }}  vertical>
+        <Card.Group stackable centered >
+        {
+          reports.map((report) => {
+            return <Card key={report._id}>
+              <Card.Content>
+                <Card.Header>
+                  {report._id}
+                </Card.Header>
+                <Card.Meta>
+                  <span>
+                    Transaction ID
+                    {/* {report.dateTransacted} */}
+                  </span>
+                </Card.Meta>
+                <Divider />
+                <Card.Description>
+                  <List animated verticalAlign='middle'>
+                    <List.Item>
+                      <List.Icon name='tag' verticalAlign='middle' />
+                      <List.Content>
+                        <List.Header>
+                          Tasker fee: 
+                        </List.Header>
+                        <List.Description>
+                          P {report.taskerFee}
+                        </List.Description>
+                      </List.Content>
+                    </List.Item>
+                    <List.Item>
+                      <List.Icon name='tag' verticalAlign='middle' />
+                      <List.Content>
+                        <List.Header>
+                          Customer fee: 
+                        </List.Header>
+                        <List.Description>
+                          P {report.customerFee}
+                        </List.Description>
+                      </List.Content>
+                    </List.Item>
+                    <List.Item>
+                      <List.Icon name='tag' verticalAlign='middle' />
+                      <List.Content>
+                        <List.Header>
+                          mnmltaskr profit: 
+                        </List.Header>
+                        <List.Description>
+                          P {report.mnmltaskrProfit}
+                        </List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <span>{report.dateTransacted}</span>
+              </Card.Content>
+            </Card>
+          })
+        }
+        </Card.Group>
+      </Segment>
     )
   }
 }
